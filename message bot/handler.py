214b2fba93ae6a3aa21398f134fc26c61f358824
Baptiste Lombard <baptiste.lombard@student.uclouvain.be>
2018-05-11 17:59:09 +0200

    new method for more clarity

--- message bot/handler.py
+++ message bot/handler.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import time
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
 
@@ -10,7 +11,9 @@ TOKEN = os.environ['TELEGRAM_TOKEN']
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 # REST API nicehash
-NICEHASH_URL = "https://api.nicehash.com/api?method=balance&id=<id>&key=<key>"
+NICEHASH_URL = "https://api.nicehash.com/api?method="
+NICEHASH_BALANCE = "balance&id=<id>&key=<key>"
+NICEHASH_PAYMENTS = "stats.provider.payments&addr=<addr>"
 headers = {'Content-Type': 'application/json'}
 
 COPAY_BALANCE = #...
@@ -24,27 +27,7 @@ def hello(event, context):
         chat_id = data["message"]["chat"]["id"]
         first_name = data["message"]["chat"]["first_name"]
 
-        if "/help" == message:
-            response = "/start to say hi \n/balance to get the total balance \n/detailed_balance to get the distribution bewteen wallets"
-        if "/start" == message:
-            response = "Hello {}".format(first_name)
-        if "/balance" == message:
-            balance = requests.get(NICEHASH_URL, headers=headers)
-            if balance.status_code == 200:
-                string = json.loads(balance.content.decode('utf-8'))
-                balance_nicehash = float(string["result"]["balance_confirmed"]) + float(string["result"]["balance_pending"])
-                total = balance_nicehash + COPAY_BALANCE + COINBASE_BALANCE
-                response = '%.8f' % total + " BTC"
-            else:
-                response = "Nicehash API error"
-        if "/detailed_balance" == message:
-            balance = requests.get(NICEHASH_URL, headers=headers)
-            if balance.status_code == 200:
-                string = json.loads(balance.content.decode('utf-8'))
-                balance_nicehash = float(string["result"]["balance_confirmed"]) + float(string["result"]["balance_pending"])
-                response = '%.8f' % balance_nicehash + " Nicehash\n" + '%.8f' % float(COPAY_BALANCE) + " copay\n" + '%.8f' % float(COINBASE_BALANCE) + " coinbase"
-            else:
-                response = "Nicehash API error"
+        response = commandHandler(message)
 
         data = {"text": response.encode("utf8"), "chat_id": chat_id}
         url = BASE_URL + "/sendMessage"
@@ -54,3 +37,39 @@ def hello(event, context):
         print(e)
 
     return {"statusCode": 200}
+
+def commandHandler(message):
+    if "/help" == message:
+        return "/hello to say hi\n/balance to get the total balance\n/detailed_balance to get the distribution bewteen wallets\n/payments to get the last payments"
+    if "/hello" == message:
+        return "Hello {}".format(first_name)
+    if "/balance" == message:
+        balance = requests.get(NICEHASH_URL+NICEHASH_BALANCE, headers=headers)
+        if balance.status_code == 200:
+            string = json.loads(balance.content.decode('utf-8'))
+            balance_nicehash = float(string["result"]["balance_confirmed"]) + float(string["result"]["balance_pending"])
+            total = balance_nicehash + COPAY_BALANCE + COINBASE_BALANCE
+            return '%.8f' % total + " BTC"
+        else:
+            return "erreur avec l'API nicehash"
+    if "/detailed_balance" == message:
+        balance = requests.get(NICEHASH_URL+NICEHASH_BALANCE, headers=headers)
+        if balance.status_code == 200:
+            string = json.loads(balance.content.decode('utf-8'))
+            balance_nicehash = float(string["result"]["balance_confirmed"]) + float(string["result"]["balance_pending"])
+            response = '%.8f' % balance_nicehash + " Nicehash\n" + '%.8f' % float(COPAY_BALANCE) + " copay\n" + '%.8f' % float(COINBASE_BALANCE) + " coinbase"
+            return response
+        else:
+            return "erreur avec l'API nicehash"
+    if "/payments" == message:
+        payments = requests.get(NICEHASH_URL+NICEHASH_PAYMENTS, headers=headers)
+        if payments.status_code == 200:
+            string = json.loads(payments.content.decode('utf-8'))
+            payments = string["result"]["payments"]
+            response = ''
+            for day_pay in payments:
+                day = time.strftime("%d/%m/%y", time.localtime(int(day_pay["time"])))
+                response += day_pay["amount"] + " BTC " + day + "\n"
+            return response
+        else:
+            return "erreur avec l'API nicehash"
